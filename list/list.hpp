ab57ef989daa32acdc644ea55c6eb0a943678c37
zevin02 <1833195090@qq.com>
2022-05-31 22:15:23 +0800

    通过增加类模板参数，实现const迭代器

--- list/list.hpp
+++ list/list.hpp
@@ -4,12 +4,12 @@ using namespace std;
 namespace xzw
 {
     template <class T>
-    struct ListNode //节点
+    struct ListNode //节点，
     {
         ListNode<T> *_prev;
         ListNode<T> *_next;
         T _data;
-        ListNode(const T &data)
+        ListNode(const T &data)//构造函数，对节点就行初始化，有参数
             : _data(data), _next(nullptr), _prev(nullptr)//其实他的头和尾指针也不用初始化，
 
         {
@@ -21,17 +21,16 @@ namespace xzw
         }
     };
 
-    template <class T>
+    template <class T,class Ref>//我们这里多增加一个模板参数,处理const迭代器
     struct _list_iterator //迭代器这个类
     {
         typedef ListNode<T> Node; //把这个对象重命名一下,这样之后就不用再显示使用T
-        typedef _list_iterator<T> _iterator;
+        typedef _list_iterator<T,Ref> _iterator;
         Node *_node;            //节点的一个迭代器,因为本质上还是指针，这个迭代器他本来就是指向一个节点，所以他必须要有一个节点的指针
         _list_iterator(Node *x) //用一个节点的指针来构造一个迭代器
             : _node(x)
-        {
-        }
-        _iterator &operator++()
+        {}
+        _iterator &operator++()//++it;
         {
             //到下一个位置
             _node = _node->_next;
@@ -43,14 +42,11 @@ namespace xzw
             _node = _node->_prev;
             return *this;
         }
-        T &operator*() //传引用返回，之后这个值还能被修改
+        Ref operator*() //传引用返回，之后这个值还能被修改.这里使用一个模板参数
         {
             return _node->_data;
         }
-        const T &operator*() //是我们的容器的const对象，而不是迭代器是const对象
-        {
-            return _node->_data;
-        }
+         
         bool operator==(const _iterator &t)//这个对象不能被改变
         {
             return _node == t._node;
@@ -66,6 +62,8 @@ namespace xzw
 
     };
 
+    //如果要实现const迭代器，我们不要写冗余的代码
+
     template <class T>
     class List
     {
@@ -73,10 +71,12 @@ namespace xzw
         typedef ListNode<T> Node; //把这个对象重命名一下,这样之后就不用再显示使用T
 
     private:
-        Node *_head; //头节点
+        Node *_head; //头节点,哨兵位
 
     public:
-        typedef _list_iterator<T> iterator; //把迭代器也重命名一下
+        typedef _list_iterator<T,T&> iterator; //把迭代器也重命名一下,普通的迭代器
+        typedef _list_iterator<T,const T&> const_iterator; //const迭代器，第二个参数使用const对象，
+
         List()                              //构造函数，里面对头节点进行初始化
         {
             _head = new Node;     //无参构造
@@ -85,7 +85,7 @@ namespace xzw
         }
         void PushBack(const T &x) //处理尾插
         {
-            Node *t = new Node(x); //开空间+初始化
+            Node *t = new Node(x); //开空间+初始化，有参数的构造
             //这个就算链表是空也是可以使用的
             Node *last = _head->_prev;
             last->_next = t;
@@ -98,11 +98,21 @@ namespace xzw
             return iterator(_head->_next); //调用了一个构造函数，使用了一个匿名对象，头节点的下一个就是第一个位置
             //返回一个迭代器的类
         }
-        iterator end() //迭代器的尾
+        iterator end() //迭代器的尾，iterator是一个类型
         {
             return iterator(_head); // end是最后一个数据的下一个位置//使用了一个匿名对象
         }
+        //上面是普通迭代器，下面是const迭代器
         
+        const_iterator begin() const //迭代器的头
+        {
+            return const_iterator(_head->_next); //调用了一个构造函数，使用了一个匿名对象，头节点的下一个就是第一个位置
+            //返回一个迭代器的类
+        }
+        const_iterator end() const//迭代器的尾，iterator是一个类型
+        {
+            return const_iterator(_head); // end是最后一个数据的下一个位置//使用了一个匿名对象
+        }
         
 
         //弄出迭代器出来,迭代器还是一个节点的指针，只不过这个迭代器用一个自定义类型进行封装
@@ -126,11 +136,12 @@ namespace xzw
     
     };
 
-    void print(const List<int>& lt)//const对象必须使用const迭代器
+    void print(const List<int>& lt)//const对象必须使用const迭代器,这里我们实现的就是使用const迭代器进行打印
     {
-         List<int>::iterator it=lt.begin();
+         List<int>::const_iterator it=lt.begin();//这里调用的就是const迭代器，实现了重载
          while(it!=lt.end())
          {
+            //  *it+=2;//这个调用的是const迭代器
              cout<<*it<<" ";
              ++it;
          }
