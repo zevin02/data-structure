ac51f8fcc448d9253d6c4a6007e3f19d673fc186
Zewen Xu <zevin9427@gmail.com>
2023-09-28 13:08:09 +0800

    implement bptree delete,Iterator and add test as well

--- bptree/bptree.go
+++ bptree/bptree.go
@@ -90,6 +90,38 @@ func (node *BPNode) addChild(child *BPNode) {
 	}
 }
 
+func (node *BPNode) deleteItem(key int64) bool {
+	num := len(node.Items)
+	for i := 0; i < num; i++ {
+		if node.Items[i].key > key {
+			//说明当前的key就不存在，删除失败
+			return false
+		} else if node.Items[i].key == key {
+			//删除当前的节点
+			copy(node.Items[i:], node.Items[i+1:])         //复制后面的节点到前面去
+			node.Items = node.Items[0 : len(node.Items)-1] //将最后一个节点元素删除
+			if len(node.Items) > 0 {
+				node.maxKey = node.Items[len(node.Items)-1].key //更新最大值
+			}
+			return true
+		}
+	}
+	return false
+}
+
+func (node *BPNode) deleteChild(child *BPNode) bool {
+	num := len(node.Nodes)
+	for i := 0; i < num; i++ {
+		if node.Nodes[i] == child {
+			copy(node.Nodes[i:], node.Nodes[i+1:])         //在父节点中删除这个孩子节点
+			node.Nodes = node.Nodes[0 : len(node.Nodes)-1] //把最后一个节点删除掉
+			node.maxKey = node.Nodes[len(node.Nodes)-1].maxKey
+			return true
+		}
+	}
+	return false
+}
+
 //BPTree B+树的定义
 type BPTree struct {
 	mutex sync.Mutex
@@ -155,6 +187,52 @@ func (t *BPTree) Get(key int64) interface{} {
 	return nil
 }
 
+func (t *BPTree) Scan(begin int64, end int64) []interface{} {
+	t.mutex.Lock()
+	defer t.mutex.Unlock()
+	node := t.root //先获得当前的根节点
+	for i := 0; i < len(node.Nodes); {
+		//遍历当前的根节点，从最小的开始进行查询
+		if begin <= node.Nodes[i].maxKey {
+			//当前的key小于就可以切换到子节点
+			node = node.Nodes[i]
+			i = 0
+			continue
+		}
+		i++
+	}
+	//这个走出来的话，就是叶子节点，因为叶子节点的没有子节点
+	if len(node.Nodes) > 0 {
+		//当前没有走到叶子节点
+		return nil
+	}
+	//在当前的叶子节点中查询数据
+	var (
+		ret []interface{}
+	)
+	for i := 0; i < len(node.Items); {
+		if node.Items[i].key >= begin && node.Items[i].key <= end {
+			//找到了起点
+			ret = append(ret, node.Items[i].val)
+		}
+		if node.Items[i].key > end {
+			break
+		}
+		if i == len(node.Items)-1 {
+			//到了最后一个元素
+			if node.Next == nil {
+				break
+			}
+			node = node.Next
+			i = 0
+			continue
+		}
+		i++
+
+	}
+	return ret
+}
+
 //Set 往B+树中插入数据
 func (t *BPTree) Set(key int64, value interface{}) {
 	t.mutex.Lock()
@@ -211,11 +289,157 @@ func (t *BPTree) splitNode(node *BPNode) *BPNode {
 		node2.Items = append(node2.Items, node.Items[t.halfw:len(node.Items)]...) //移动后面一半的节点到node2中
 		node2.maxKey = node2.Items[len(node2.Items)-1].key                        //最大值，就是最后一个元素的值了
 		//修改原来节点的数据
-		node.Next = node2 //指向下一个个节点
+		//叶子节点连成一个链表
+		node2.Next = node.Next //
+		node.Next = node2      //指向下一个个节点
 		node.Items = node.Items[0:t.halfw]
 		node.maxKey = node.Items[len(node.Items)-1].key //更新当前节点的最大值
 		return node2
 	}
 	//当前元素不需要进行更新
 	return nil
+}
+
+func (t *BPTree) Remove(key int64) {
+	t.mutex.Lock()
+	defer t.mutex.Unlock()
+	t.deleteItem(nil, t.root, key)
+}
+
+func (t *BPTree) deleteItem(parent *BPNode, node *BPNode, key int64) {
+	for i := 0; i < len(node.Nodes); i++ {
+		if key <= node.Nodes[i].maxKey || i == len(node.Nodes)-1 { //遍历到了最后一个都没有，就进入到最后一个节点中
+			t.deleteItem(node, node.Nodes[i], key) //低归的进入到这个函数中进行添加节点
+			break
+		}
+	}
+	if len(node.Nodes) < 1 {
+		//当前低归到了叶子节点,没有子节点
+		node.deleteItem(key)
+		if len(node.Items) < t.halfw {
+			//删除当前的节点后，如果当前的节点的key数量<[m/2],就需要借兄弟的，或者和兄弟合并
+			t.itemMoveOrMerge(parent, node)
+		}
+		//删除当前的节点后，如果当前的节点的key数量>=[m/2],就删除完毕
+
+	} else {
+		//当前的非叶子节点
+		node.maxKey = node.Nodes[len(node.Nodes)-1].maxKey //更新当前节点的最大值
+		if len(node.Nodes) < t.halfw {
+			//当前的节点的
+			t.childMoveOrMerge(parent, node)
+		}
+
+	}
+}
+
+//对叶子节点进行移动或者合并
+func (t *BPTree) itemMoveOrMerge(parent *BPNode, node *BPNode) {
+	//先获得兄弟节点
+	var left *BPNode = nil  //如果左边没有就不需要去得到
+	var right *BPNode = nil //如果右边没有数据就不需要去得到
+	for i := 0; i < len(parent.Nodes); i++ {
+		if parent.Nodes[i] == node {
+			if i < len(parent.Nodes)-1 {
+				right = parent.Nodes[i+1]
+			}
+			if i > 0 {
+				left = parent.Nodes[i-1]
+			}
+			break
+		}
+	}
+	//将左边的节点取一个记录移动到被删除的节点
+	if left != nil && len(left.Items) > t.halfw {
+		//左边的节点存在，同时删除一个元素不会影响到树的结构
+		item := left.Items[len(left.Items)-1]              //把最后一个节点取出来
+		left.Items = left.Items[0 : len(left.Items)-1]     //删除当前的节点
+		left.maxKey = left.Items[len(left.Items)-1].key    //更新左边节点的最大值
+		node.Items = append([]BPItem{item}, node.Items...) //左边的节点一定是小于被删除的节点的，所以需要被头插
+		return
+	}
+	//将右侧节点取一个记录到被删除的节点
+	if right != nil && len(right.Items) > t.halfw {
+		//右边节点取出一个，一定比被删除的节点要大，所以取出第一个
+		item := right.Items[0]
+		right.Items = right.Items[1:]         //删除当前的节点
+		node.Items = append(node.Items, item) //右边节点一定是比被删除节点大的，所以要尾插
+		node.maxKey = node.Items[len(node.Items)-1].key
+		return
+	}
+	//合并都是向左合并
+	//与左侧节点进行合并,把当前这个节点合并到左侧节点来
+	if left != nil && len(left.Items)+len(node.Items) < t.width {
+		left.Items = append(left.Items, node.Items...)
+		left.Next = node.Next
+		left.maxKey = left.Items[len(left.Items)-1].key
+		//在父节点中删除当前的这个节点
+		parent.deleteChild(node)
+		return
+	}
+	//与右侧节点进行合并，把右侧节点移动到当前这个被删除的节点来
+	if right != nil && len(right.Items)+len(node.Items) < t.width {
+		node.Items = append(node.Items, right.Items...) //把右边节点合并到当前的节点
+		//把节点进行删除
+		node.Next = right.Next
+		node.maxKey = node.Items[len(node.Items)-1].key
+		parent.deleteChild(right)
+		return
+	}
+
+}
+
+func (t *BPTree) childMoveOrMerge(parent *BPNode, node *BPNode) {
+	if parent == nil {
+		return
+	}
+	var left *BPNode = nil  //如果左边没有就不需要去得到
+	var right *BPNode = nil //如果右边没有数据就不需要去得到
+	for i := 0; i < len(parent.Nodes); i++ {
+		if parent.Nodes[i] == node {
+			if i < len(parent.Nodes)-1 {
+				right = parent.Nodes[i+1]
+			}
+			if i > 0 {
+				left = parent.Nodes[i-1]
+			}
+			break
+		}
+	}
+
+	//将左边的节点取一个记录移动到被删除的节点
+	if left != nil && len(left.Nodes) > t.halfw {
+		//左边的节点存在，同时删除一个元素不会影响到树的结构
+		item := left.Nodes[len(left.Nodes)-1]               //把最后一个节点取出来
+		left.Nodes = left.Nodes[0 : len(left.Nodes)-1]      //删除当前的节点
+		node.Nodes = append([]*BPNode{item}, node.Nodes...) //左边的节点一定是小于被删除的节点的，所以需要被头插
+		left.maxKey = left.Nodes[len(left.Nodes)-1].maxKey
+		return
+	}
+	//将右侧节点取一个记录到被删除的节点
+	if right != nil && len(right.Nodes) > t.halfw {
+		//右边节点取出一个，一定比被删除的节点要大，所以取出第一个
+		item := right.Nodes[0]                //获得右节点的第一个节点元素
+		right.Nodes = right.Nodes[1:]         //删除当前的节点
+		node.Nodes = append(node.Nodes, item) //右边节点一定是比被删除节点大的，所以要尾插
+		node.maxKey = node.Nodes[len(node.Nodes)-1].maxKey
+		return
+	}
+	//与左侧节点进行合并
+	if left != nil && len(left.Nodes)+len(node.Nodes) < t.width {
+		left.Nodes = append(left.Nodes, node.Nodes...)
+		left.maxKey = left.Nodes[len(left.Nodes)-1].maxKey
+		//在父节点中删除当前的这个节点
+		parent.deleteChild(node)
+		return
+	}
+	//与右侧节点进行合并
+	if right != nil && len(right.Nodes)+len(node.Nodes) < t.width {
+		node.Nodes = append(node.Nodes, right.Nodes...) //把右边节点合并到当前的节点
+		//把节点进行删除,todo 在这个节点上进行更新maxkey
+		node.maxKey = node.Nodes[len(node.Nodes)-1].maxKey
+		parent.deleteChild(right)
+		return
+	}
+
 }
--- bptree/bptree_test.go
+++ bptree/bptree_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestNewBPTree(t *testing.T) {
+func TestNewBPTreeSet(t *testing.T) {
 	bpt := NewBPTree(4)
 	bpt.Set(10, 1)
 	bpt.Set(23, 1)
@@ -28,9 +28,155 @@ func TestNewBPTree(t *testing.T) {
 	bpt.Set(14, 1)
 	bpt.Set(14, 15)
 	//fmt.Println(bt)
+	assert.Equal(t, 2, len(bpt.root.Nodes))
+	assert.Equal(t, int64(12), bpt.root.Nodes[0].maxKey)
+	assert.Equal(t, int64(45), bpt.root.Nodes[1].maxKey)
+	assert.Equal(t, int64(10), bpt.root.Nodes[0].Nodes[0].maxKey)
+	assert.Equal(t, int64(12), bpt.root.Nodes[0].Nodes[0].Next.maxKey)
+	assert.Equal(t, int64(15), bpt.root.Nodes[0].Nodes[0].Next.Next.maxKey)
+	assert.Equal(t, int64(33), bpt.root.Nodes[0].Nodes[0].Next.Next.Next.maxKey)
+	assert.Equal(t, int64(45), bpt.root.Nodes[0].Nodes[0].Next.Next.Next.Next.maxKey)
+	bpt.Set(46, 15)
+	bpt.Set(37, 15)
+	assert.Equal(t, int64(46), bpt.root.Nodes[1].maxKey)
+	bpt.Set(38, 15)
+	bpt.Set(40, 15)
+	assert.Equal(t, int64(12), bpt.root.Nodes[0].maxKey)
+	assert.Equal(t, int64(33), bpt.root.Nodes[1].maxKey)
+	assert.Equal(t, int64(46), bpt.root.Nodes[2].maxKey)
+	bpt.Set(47, 15)
+	bpt.Set(48, 15)
+	assert.Equal(t, int64(48), bpt.root.Nodes[2].maxKey)
+	assert.Equal(t, 6, len(bpt.Scan(9, 16)))
 	t.Log(bpt.Get(10))
 	t.Log(bpt.Get(15))
 	t.Log(bpt.Get(20))
 	t.Log(bpt.Get(14))
+	t.Log(bpt.Get(37))
+
+}
+
+func TestNewBPTreeDelete1(t *testing.T) {
+	bpt := NewBPTree(4)
+	bpt.Set(10, 1)
+	bpt.Set(23, 1)
+	bpt.Set(33, 1)
+	bpt.Set(35, 1)
+	bpt.Set(15, 1)
+	bpt.Set(2, 1)
+	bpt.Set(13, 1)
+	bpt.Set(39, 1)
+	bpt.Remove(33)
+	assert.Equal(t, 3, len(bpt.root.Nodes[1].Items))
+	bpt.Remove(39)
+	assert.Equal(t, 2, len(bpt.root.Nodes[1].Items))
+	assert.Equal(t, int64(35), bpt.root.maxKey)
+	bpt.Remove(35)
+	assert.Equal(t, int64(23), bpt.root.maxKey)
+	bpt.Remove(13)
+	assert.Equal(t, int64(10), bpt.root.Nodes[0].maxKey)
+	bpt.Remove(10)
+	assert.Equal(t, int64(23), bpt.root.Nodes[0].maxKey)
+	assert.Equal(t, 1, len(bpt.root.Nodes))
+	assert.Nil(t, bpt.Get(10))
+	assert.NotNil(t, bpt.Get(23))
+	assert.Equal(t, 1, bpt.Get(23))
+}
+
+func TestNewBPTreeDelete2(t *testing.T) {
+	bpt := NewBPTree(4)
+	bpt.Set(10, 1)
+	bpt.Set(23, 1)
+	bpt.Set(33, 1)
+	bpt.Set(35, 1)
+	bpt.Set(15, 1)
+	bpt.Set(2, 1)
+	bpt.Set(13, 1)
+	bpt.Set(39, 1)
+	bpt.Set(12, 1)
+	bpt.Set(45, 1)
+	bpt.Set(11, 1)
+	bpt.Set(14, 1)
+	bpt.Set(14, 15)
+	bpt.Set(46, 15)
+	bpt.Set(37, 15)
+	bpt.Set(38, 15)
+	bpt.Set(40, 15)
+	bpt.Set(47, 15)
+	bpt.Set(48, 15)
+	bpt.Remove(45)
+	assert.Equal(t, int64(46), bpt.root.Nodes[2].Nodes[2].maxKey)
+	assert.Equal(t, int64(40), bpt.root.Nodes[2].Nodes[2].Items[0].key)
+	assert.Equal(t, int64(47), bpt.root.Nodes[2].Nodes[3].Items[0].key)
+	bpt.Remove(46)
+	assert.Equal(t, 3, len(bpt.root.Nodes[2].Nodes))
+	assert.Equal(t, int64(48), bpt.root.Nodes[2].Nodes[2].maxKey)
+	assert.Equal(t, int64(47), bpt.root.Nodes[2].Nodes[2].Items[0].key)
+	assert.Equal(t, 4, len(bpt.Scan(36, 41)))
+	//借右兄弟的一个key过来
+	bpt.Remove(37)
+	//和右兄弟合并,第二层也和右节点进行合并
+	bpt.Remove(10)
+	assert.Equal(t, 2, len(bpt.root.Nodes))
+	assert.Equal(t, 3, len(bpt.root.Nodes[0].Nodes))
+	assert.Equal(t, int64(33), bpt.root.Nodes[0].maxKey)
+	//与左节点进行合并
+	bpt.Remove(48)
+	assert.Equal(t, int64(47), bpt.root.maxKey)
+	assert.Equal(t, 2, len(bpt.root.Nodes[1].Nodes))
+	bpt.Remove(38)
+	assert.Equal(t, int64(39), bpt.root.Nodes[1].Nodes[0].maxKey)
+	//第二层节点向左边借一个node
+	bpt.Remove(47)
+	assert.Equal(t, int64(15), bpt.root.Nodes[0].maxKey)
+	assert.Equal(t, int64(40), bpt.root.Nodes[1].maxKey)
+	assert.Equal(t, 2, len(bpt.root.Nodes[0].Nodes))
+	assert.Equal(t, 2, len(bpt.root.Nodes[1].Nodes))
+	bpt.Remove(33)
+	bpt.Remove(40)
+	assert.Equal(t, 1, len(bpt.root.Nodes))
+	assert.Equal(t, 3, len(bpt.root.Nodes[0].Nodes))
+	assert.Equal(t, int64(39), bpt.root.Nodes[0].maxKey)
+	bpt.Set(40, 1)
+	assert.Equal(t, int64(40), bpt.root.maxKey)
+	bpt.Set(41, 1)
+	assert.Equal(t, int64(41), bpt.root.maxKey)
+	assert.Equal(t, 4, len(bpt.root.Nodes[0].Nodes))
+	bpt.Set(16, 1)
+	assert.Equal(t, 12, len(bpt.Scan(1, 42)))
+}
+func TestNewBPTreeDelete3(t *testing.T) {
+	tree := NewBPTree(3)
+	for i := int64(1); i <= 10; i++ {
+		tree.Set(i, i)
+	}
+	// 删除叶子节点，不需要合并
+	tree.Remove(1)
+	if tree.Get(1) != nil {
+		t.Errorf("Delete failed, expected nil but got %v", tree.Get(1))
+	}
+
+	// 删除叶子节点，需要合并
+	tree.Remove(2)
+	if tree.Get(2) != nil {
+		t.Errorf("Delete failed, expected nil but got %v", tree.Get(2))
+	}
+
+	// 删除内部节点，需要合并
+	tree.Remove(3)
+	if tree.Get(3) != nil {
+		t.Errorf("Delete failed, expected nil but got %v", tree.Get(3))
+	}
+
+	// 删除内部节点，不需要合并
+	tree.Remove(4)
+	if tree.Get(4) != nil {
+		t.Errorf("Delete failed, expected nil but got %v", tree.Get(4))
+	}
+
+	// 删除后树为空
+	for i := int64(5); i <= 10; i++ {
+		tree.Remove(i)
+	}
 
 }
